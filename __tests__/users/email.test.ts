7ea51117fbfa1ea66e03d8b93c62b54b4b523e53
Marta Probierz <marta.anna.probierz@gmail.com>
2022-03-07 13:31:35 +0100

    bugfix: add testTimeout, fix assertions

--- __tests__/users/email.test.ts
+++ __tests__/users/email.test.ts
@@ -3,31 +3,27 @@ import userModel from "../../models/user.model";
 
 describe("sendEmail", () => {
   it("should send email", async () => {
-    jest.setTimeout(30000);
     const token = new userModel().generateAuthToken();
     const email = "test@test.com";
     const res = await sendEmail(email, token);
-    console.log(res);
     expect(res).not.toBeNull();
     expect(res).not.toBeUndefined();
-    expect(res).toBe("Mail send!");
+    expect(res).toBe("Mail has been sent!");
   });
 });
 
 describe("error", () => {
   it("tests error with async/await", async () => {
     try {
-      jest.setTimeout(30000);
       const token = new userModel().generateAuthToken();
       const email = "test@test.com";
       const res = await sendEmail(email, token);
-      console.log(res);
     } catch (e) {
       expect(e).toEqual({
-        error: "Mail doesn't send!",
+        error: "Something is wrong!",
       });
       expect(e).toMatch('error');
-      expect.assertions(1);
+      expect.assertions(2);
     }
   });
 });
