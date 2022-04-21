a5b89f612de40273eb54ecd7347c1fd9029c17a3
Anna Koruc <aniakoruc@gmail.com>
2022-04-21 21:17:23 +0200

    emailVerification server

--- src/users/confirmation.ts
+++ src/users/confirmation.ts
@@ -1,23 +1,23 @@
-import { Request, Response } from 'express'
-import { StatusCodes } from 'http-status-codes'
-import jwt from 'jsonwebtoken'
-import mongoose from 'mongoose'
-import config from 'config'
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import config from "config";
 
-import userModel from '../../models/user.model'
-import User from '../../interfaces/user.interface'
+import userModel from "../../models/user.model";
+import User from "../../interfaces/user.interface";
 
 const confirmUser = async (req: Request, res: Response) => {
   try {
-    const userInfo = (await jwt.verify(req.params.token, config.get('jwtPrivateKey'))) as User & mongoose.Document<any>
+    const userInfo = (await jwt.verify(req.params.token, config.get("jwtPrivateKey"))) as User & mongoose.Document<any>;
 
-    const user = await userModel.findByIdAndUpdate(userInfo._id, { isVerified: true }, { new: true })
-    if (!user) return res.status(StatusCodes.NOT_FOUND).send('User not found')
+    const user = await userModel.findByIdAndUpdate(userInfo._id, { isVerified: true }, { new: true });
+    if (!user) return res.status(StatusCodes.NOT_FOUND).send("User not found");
 
-    res.status(StatusCodes.OK).redirect(`http://${process.env.ADDRESSPORT}/login`)
+    res.status(StatusCodes.OK).redirect(`http://${process.env.URL}/email-verification`);
   } catch (ex) {
-    res.status(StatusCodes.BAD_REQUEST).send('Invalid token.')
+    res.status(StatusCodes.BAD_REQUEST).send("Invalid token.");
   }
-}
+};
 
-export default confirmUser
+export default confirmUser;
