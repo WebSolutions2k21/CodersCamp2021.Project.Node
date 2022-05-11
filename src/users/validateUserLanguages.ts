e3d6c80b0665c283861c7b925a686ead5326a4e7
Aleksander <aleksander.atamanczuk@gmail.com>
2022-05-11 22:17:21 +0200

    refactor language validation

--- src/users/validateUserLanguages.ts
+++ src/users/validateUserLanguages.ts
@@ -2,10 +2,11 @@ import Joi from "joi";
 
 import User from "../../interfaces/user.interface";
 import LANG_LEVEL from "../../enums/userLanguages";
+import ProgrammingLanguageArr from "../../interfaces/programmingLanguage.interface";
 
 export default function validateUserLanguages(user: User) {
   const schema = Joi.object({
-    programmingLanguage: Joi.array().default({
+    programmingLanguage: Joi.array().items({
       nameLang: Joi.string().max(100),
       level: Joi.string().valid(LANG_LEVEL.JUNIOR, LANG_LEVEL.MID, LANG_LEVEL.SENIOR),
     }),
