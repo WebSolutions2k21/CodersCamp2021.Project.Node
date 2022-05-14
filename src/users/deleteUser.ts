a150a836291c5dd45a48fa16250efb14f716a58e
Aleksander <aleksander.atamanczuk@gmail.com>
2022-05-14 15:29:26 +0200

    delete all projcects created by user

--- src/users/deleteUser.ts
+++ src/users/deleteUser.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import { projectModel } from "../../models/project.model";
 import userModel from "../../models/user.model";
 
 const deleteUser = async (req: Request, res: Response) => {
-  const user = await userModel.findByIdAndDelete(req.params.id);
+  const user = await userModel.findByIdAndDelete(req.userInfo._id);
   if (!user) return res.status(StatusCodes.NOT_FOUND).send("No user found");
 
-  return res.status(StatusCodes.OK).send(user);
+  await projectModel.deleteMany({ userId: req.userInfo._id });
+
+  return res.status(StatusCodes.OK).send(`Delete user with id: ${user._id}`);
 };
 
 export default deleteUser;
